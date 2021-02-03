599233e5dd50c1acb542c203385c6e1753d23f63
Bjoern Meyer <bjoern@textcontrol.com>
2021-02-03 23:56:12 +0100

    initial upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace tx_zugferd {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.textControl1 = new TXTextControl.TextControl();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textControl1
+			// 
+			this.textControl1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.textControl1.Font = new System.Drawing.Font("Arial", 10F);
+			this.textControl1.Location = new System.Drawing.Point(0, 0);
+			this.textControl1.Name = "textControl1";
+			this.textControl1.Size = new System.Drawing.Size(800, 363);
+			this.textControl1.TabIndex = 0;
+			this.textControl1.Text = "textControl1";
+			this.textControl1.UserNames = null;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 381);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(164, 57);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Create Demo Invoice";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.textControl1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Creating ZUGFeRD Invoices";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private TXTextControl.TextControl textControl1;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TXTextControl.DocumentServer.PDF.Zugferd;
+
+namespace tx_zugferd {
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+		}
+
+      private Invoice CreateSampleInvoice() {
+         // new zugferd invoice
+         Invoice invoice = new Invoice("A12345", DateTime.Now, CurrencyCode.USD);
+
+         invoice.Type = InvoiceType.Invoice;
+         invoice.Profile = Profile.Comfort;
+
+         // buyer
+         invoice.Buyer = new TradeParty {
+            ID = "TX_1",
+            Name = "Text Control GmbH",
+            ContactName = "Peter Paulsen",
+            City = "Bremen",
+            Postcode = "28217",
+            Country = CountryCode.DE,
+            Street = "Überseetor 18"
+         };
+
+         // seller
+         invoice.Seller = new TradeParty {
+            ID = "TX_2",
+            Name = "Text Control, LLC",
+            ContactName = "Jack Jackson",
+            City = "Charlotte, NC",
+            Postcode = "28210",
+            Country = CountryCode.US,
+            Street = "6926 Shannon Willow Rd, Suite 400",
+         };
+
+         // add tax id's
+         invoice.Seller.SpecifiedTaxRegistrations.Add(
+            new TaxID() { ID = "US12367623", Scheme = TaxScheme.VA });
+
+         // add products
+         List<LineItem> lineItems = new List<LineItem>();
+
+         lineItems.Add(new LineItem() {
+            Price = 200,
+            ProductID = "A123",
+            Name = "Product A",
+            Quantity = 5,
+            Total = 1000,
+            UnitCode = QuantityCodes.C62
+         });
+
+         // add line items to invoice
+         foreach (LineItem item in lineItems)
+            invoice.LineItems.Add(item);
+
+         // set the total amount
+         invoice.TotalAmount = 1000;
+
+         return invoice;
+      }
+
+		private void button1_Click(object sender, EventArgs e) {
+
+         TXTextControl.DocumentServer.MailMerge mm = new TXTextControl.DocumentServer.MailMerge();
+         mm.TextComponent = textControl1;
+
+         textControl1.Load("invoice.tx", TXTextControl.StreamType.InternalUnicodeFormat);
+
+         Invoice invoice = CreateSampleInvoice();
+
+         // merge data into template
+         mm.MergeJsonData(JsonConvert.SerializeObject(invoice));
+
+         // create the XML
+         string xmlZugferd = invoice.CreateXml();
+         
+         // get the required meta data
+         string metaData = MetaData.GetMetaData();
+
+			TXTextControl.SaveSettings saveSettings = new TXTextControl.SaveSettings();
+
+         // create a new embedded file
+         var zugferdInvoice = new TXTextControl.EmbeddedFile(
+            "ZUGFeRD-invoice.xml",
+            Encoding.UTF8.GetBytes(xmlZugferd),
+            metaData);
+
+         zugferdInvoice.Description = "ZUGFeRD-invoice";
+         zugferdInvoice.Relationship = "Alternative";
+         zugferdInvoice.MIMEType = "application/xml";
+         zugferdInvoice.LastModificationDate = DateTime.Now;
+
+         // set the embedded files
+         saveSettings.EmbeddedFiles = new TXTextControl.EmbeddedFile[] {
+            new TXTextControl.EmbeddedFile(
+               "ZUGFeRD-invoice.xml",
+               Encoding.UTF8.GetBytes(xmlZugferd),
+               metaData) };
+
+         // export the PDF
+			textControl1.Save("test.pdf", TXTextControl.StreamType.AdobePDFA, saveSettings);
+		}
+	}
+}
